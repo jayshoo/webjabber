b0a9f0c67a25f74cc74c48923c3a25c6d4b55c1b
Joshua McLean <jayshoo@gmail.com>
2011-11-26 09:40:12 +1100

    work

--- webjabber.js
+++ webjabber.js
@@ -1,24 +1,87 @@
 // http://xmpp.org/extensions/xep-0045.html
 var xmpp = require('node-xmpp');
+var qs = require('querystring');
 var http = require('http');
 
-/*
-var me = 'jbot@joshu.net/jbot';
-var password = 'jbotpassword';
-var nick = 'jbot';
-var room = 'bowels@conference.vmx9155.hosting24.com.au';
+function login(args) {
+  var keys = ['jid', 'password', 'room', 'nick'];
+  for (ind in keys) {
+    if (args[keys[ind]] === undefined)
+      return undefined;
+  }
 
-var conferenceMe = room + '/' + nick;
-*/
+  var cl = new xmpp.Client({
+    jid: args.jid,
+    password: args.password
+  });
 
-function checkPostedLogin(args) {
-  if (args['jid'] === undefined) return undefined;
+  // send presence to the desired chat asap
+  cl.on('online', function() {
+    cl.send(new xmpp.Element('presence', {
+      to: args.room+'/'+args.nick
+    }));
+  });
+  cl.on('error', function(error) {
+    console.log(error);
+  });
+
+  return cl;
+}
+
+function initialConnection(client, req, res) {
+  var query = '';
+  req.on('data', function(data) {
+    query += data;
+  });
+  req.on('end', function() {
+    var args = qs.parse(query);
+
+    client = login(args);
+    if (client === undefined) {
+      console.log('client was undefined');
+      console.log(args);
+
+      res.writeHead(400);
+      res.end();
+      return;
+    }
+
+    handleMessages(client, req, res);
+  });
+
+  res.writeHead(200, {
+    'Transfer-Encoding': 'chunked'
+  });
+}
+
+function handleMessages(client, req, res) {
+  var query = '';
+  req.on('data', function(data) {
+    query += data;
+  });
+  req.on('end', function() {
+    var args = qs.parse(query);
+    query = '';
+  });
+  client.on('stanza', function(stanza) {
+    if (stanza.is('message')
+    && stanza.attrs.type !== 'error') {
+      var body = stanza.getChildText('body');
+      if (body !== null) {
+        console.log(body);
+        res.write(body);
+      }
+    }
+  });
 }
 
 http.createServer(function (req, res) {
-  console.log(req);
+  var client;
+  initialConnection(client, req, res);
+
   res.end();
 }).listen(8005);
+
 
 /*
 var cl = new xmpp.Client({
