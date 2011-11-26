ec8a175b290e217f48cb4b66f3dd2c7120032458
Joshua McLean <jayshoo@gmail.com>
2011-11-26 11:51:12 +1100

    First working version

--- webjabber.js
+++ webjabber.js
@@ -1,100 +1,81 @@
 // http://xmpp.org/extensions/xep-0045.html
 var xmpp = require('node-xmpp');
-var qs = require('querystring');
-var http = require('http');
+var express = require('express');
+var app = express.createServer();
+var io = require('socket.io').listen(app);
 
-function login(args) {
-  var keys = ['jid', 'password', 'room', 'nick'];
-  for (ind in keys) {
-    if (args[keys[ind]] === undefined)
-      return undefined;
-  }
 
-  var cl = new xmpp.Client({
-    jid: args.jid,
-    password: args.password
-  });
+app.listen(8000);
 
-  // send presence to the desired chat asap
-  cl.on('online', function() {
-    cl.send(new xmpp.Element('presence', {
-      to: args.room+'/'+args.nick
-    }));
-  });
-  cl.on('error', function(error) {
-    console.log(error);
-  });
+app.configure(function() {
+  app.use(express.static(__dirname + '/static'));
+  app.set('views', __dirname);
+  app.set('view engine', 'jade');
+});
 
-  return cl;
-}
+app.get('/', function(req, res) {
+  res.render('index', { layout: false });
+});
 
-function initialConnection(client, req, res) {
-  var query = '';
-  req.on('data', function(data) {
-    query += data;
-  });
-  req.on('end', function() {
-    var args = qs.parse(query);
 
-    client = login(args);
-    if (client === undefined) {
-      console.log('client was undefined');
-      console.log(args);
+io.sockets.on('connection', function(socket) {
 
-      res.writeHead(400);
-      res.end();
-      return;
-    }
+  socket.on('login', function(jid, password, room, nick) {
+    var client = new xmpp.Client({
+      jid: jid,
+      password: password
+    });
+    client.on('error', function(error) {
+      console.log(error);
+      socket.emit('error', error);
+    });
+    client.on('online', function() {
+      client.send(new xmpp.Element('presence', {
+        to: room+'/'+nick
+      }));
+    });
+    client.on('stanza', function(stanza) {
+      if (stanza.is('message')
+      && stanza.attrs.type !== 'error')
+      {
+        var from = stanza.attrs.from;
+        var body = stanza.getChildText('body');
 
-    handleMessages(client, req, res);
-  });
-
-  res.writeHead(200, {
-    'Transfer-Encoding': 'chunked'
-  });
-}
-
-function handleMessages(client, req, res) {
-  var query = '';
-  req.on('data', function(data) {
-    query += data;
-  });
-  req.on('end', function() {
-    var args = qs.parse(query);
-    query = '';
-  });
-  client.on('stanza', function(stanza) {
-    if (stanza.is('message')
-    && stanza.attrs.type !== 'error') {
-      var body = stanza.getChildText('body');
-      if (body !== null) {
-        console.log(body);
-        res.write(body);
+        socket.emit('message', from, body);
       }
-    }
+    });
+
+    socket.set('info', {
+      client: client,
+      jid: jid,
+      password: password,
+      room: room,
+      nick: nick
+    });
   });
-}
 
-http.createServer(function (req, res) {
-  var client;
-  initialConnection(client, req, res);
+  socket.on('sendmessage', function(text) {
+    socket.get('info', function(err, info) {
+      var msg = new xmpp.Element('message', {
+        from: info.from,
+        to: info.room,
+        type: 'groupchat'
+      }).c('body').t(text);
 
-  res.end();
-}).listen(8005);
+      info.client.send(msg);
+    });
+  });
+
+  socket.on('disconnect', function() {
+    socket.get('info', function(err, info) {
+      info.client.end();
+    });
+  });
+
+});
 
 
 /*
-var cl = new xmpp.Client({
-  jid: me,
-  password: password
-});
-
-cl.on('online', function() {
-  cl.send(new xmpp.Element('presence', {
-    to: conferenceMe
-  }));
-});
-
 cl.on('stanza', function(stanza) {
   if (stanza.is('message')
     && stanza.attrs.type !== 'error'
@@ -113,4 +94,6 @@ cl.on('stanza', function(stanza) {
   }
 });
 */
+
+
 
